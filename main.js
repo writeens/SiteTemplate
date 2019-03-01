87b6c4d5f006a19056bf7095a44c657e7a528df3
writeens <44687555+writeens@users.noreply.github.com>
2019-03-01 22:39:56 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,13 +7,7 @@ items.forEach(item => item.addEventListener("transitionend", updateText));
 function updateText(e) {
     if (e.propertyName.includes("flex")) {
         this.classList.toggle("open");
-        this.classList.toggle("black");
-        items.forEach((item) => {
-            if (item !== this) {
-                item.classList.remove("black");
-                item.classList.toggle("opaque");
-            }
-        });
+        this.classList.toggle("white");
     }
 }
 function updateFlex() {
